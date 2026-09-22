f3956118538535f62ce2cf4422ec426b8fa62b95
tukemen-rgb <tukemen@ramenbu.com>
2026-09-22 22:58:18 +1200

    Unify cancellation recovery page teardown

--- prototypes/tests/role-handover.test.cjs
+++ prototypes/tests/role-handover.test.cjs
@@ -1,4 +1,4 @@
-YªçŠx-®éÜj×¢ëiºÚ+Š§j[h‘éÜ¢éí×ŸxëDèµ©hºÚn¶X§zÍIİ\ÙHİšXİ	ÎÂ‹ËÈXØÙ\[˜ÙH™]šY]ÈÙˆHİ\YYS›İ›ÙXİ[Ûˆ]]Üš^˜][Ûˆ\İË‚˜ÛÛœİİ\İHH™\]Z\™J	Û›ÙN\İ	ÊNÂ˜ÛÛœİ\ÜÙ\H™\]ZuçİúÒÚ$z{-®éÜj×tted,false);
+YªçŠx-®éÜj×¢ëiºÚ+Š§j[h‘éÜ¢éí×­9çDèµ©hºÚn¶X§zÍIİ\ÙHİšXİ	ÎÂ‹ËÈXØÙ\[˜ÙH™]šY]ÈÙˆHİ\YYS›İ›ÙXİ[Ûˆ]]Üš^˜][Ûˆ\İË‚˜ÛÛœİİ\İHH™\]Z\™J	Û›ÙN\İ	ÊNÂ˜ÛÛœİ\ÜÙ\H™\]ZuëN9ÒÚ$z{-®éÜj×tted,false);
   assert.equal(staleSession.reason,'stale_session');
   assert.equal(staleSession.resent,false);
   assert.equal(staleReplayCalls,0);
